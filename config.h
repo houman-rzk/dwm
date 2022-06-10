20ac02488289e2920d8563b2511764a201f37af1
ache <ache@ache.com>
2022-06-10 09:25:45 +0200

    Applied 'scratchpads' patch and changed some shortcuts

--- config.h
+++ config.h
@@ -1,12 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
+/* Constants */
+#define TERMINAL "st"
+//#define TERMCLASS "St"
+
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 128;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int snap      = 16;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -24,6 +28,20 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_scarlet },
 };
 
+typedef struct {
+	const char *name;
+	const void *cmd;
+} Sp;
+const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-g", "50x20",  "-f", "monospace:size=16", "-e", "bc", "-lq", NULL };
+//const char *spcmd3[] = {"keepassxc", NULL };
+static Sp scratchpads[] = {
+	/* name          cmd  */
+	{"spterm",      spcmd1},
+	{"spcalc",      spcmd2},
+	//{"keepassxc",   spcmd3},
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -33,12 +51,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	/* class      	     instance    title    tags mask     isfloating   CenterThisWindow?     monitor */
-//	{ "st",              NULL,       NULL,    0,            0,     	     1,		           -1 },
-//	{ "Gimp",            NULL,       NULL,    0,            1,           0,                    -1 },
-//	{ "Firefox",         NULL,       NULL,    1 << 8,       0,           0,                    -1 },
+	{ "Gimp",     NULL,	  NULL,	      1 << 8,	    0,		 -1 },
+	{ TERMINAL,   NULL,	  NULL,	      0,	    0,		 -1 },
+	{ "Firefox",  NULL,	  NULL,	      1 << 8,	    0,		 -1 },
+	{ TERMINAL,   "spterm",	  NULL,	      SPTAG(0),	    1,		 -1 },
+	{ TERMINAL,   "spcalc",	  NULL,	      SPTAG(1),	    1,		 -1 },
+	//{ TERMINAL,   "keepassxc",NULL,	      SPTAG(2),	    0,		 -1 },
 };
 
 /* layout(s) */
@@ -50,7 +68,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 
-/* Never used. Here only to avoid compiling warnings */
+/* Never used. Here only to avoid compiling warnings when commenting out Layout in layouts[] */
 static const Layout all[]={{"[]=",tile },{"[M]",monocle },{"[@]",spiral },{"[\\]",dwindle},{"H[]",deck},{"TTT",bstack},{"===",bstackhoriz},{"HHH",grid},{"###",nrowgrid},{"---",horizgrid},{":::",gaplessgrid},{"|M|",centeredmaster},{">M>",centeredfloatingmaster},{"><>",NULL},{NULL, NULL},};
 
 
@@ -88,13 +106,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -104,9 +122,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_minus,  setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_plus,   setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+//	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,			XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY,		        XK_period, cyclelayout,    {.i = +1 } },
+	{ MODKEY|ShiftMask,		XK_s,  	   togglescratch,  {.ui = 0 } },
+	{ MODKEY|ShiftMask,		XK_c,  	   togglescratch,  {.ui = 1 } },
+//	{ MODKEY,      			XK_u,	   togglescratch,  {.ui = 1 } },
+//	{ MODKEY,      			XK_x,	   togglescratch,  {.ui = 2 } },
+
 //	{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
 //	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
 //	{ MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
@@ -124,7 +147,7 @@ static Key keys[] = {
 //	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
 //	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,	                XK_c,      killclient,     {0} },
 	{ LEFTALT|ShiftMask,            XK_x,      killunsel,      {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 //	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
